703f4c2b92a47c19cd12f2636757e77a4d79c6b5
tomasstauber <tomystauber@gmail.com>
2022-12-14 18:36:16 -0300

    correcciones de adaptabilidad, agregué el spinner de carga

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const loaderContainer = document.querySelector('.loading');
+let inicio = new Date;
+
+window.addEventListener('load', () => {
+    
+    let fin = new Date;
+    let segundos = (fin-inicio);
+
+    if(segundos < 2000){
+        setTimeout(() => {
+            loaderContainer.style.display = 'none';
+        }, 2000);
+    }else{
+        loaderContainer.style.display = 'none';
+    }
+   console.log(segundos)
+    
+});
